4c427edb7aad5620969a11354a8e886004adfb05
kiranraou <85458059+kiranraou@users.noreply.github.com>
2021-07-15 00:10:31 -0700

    Add files via upload

---
+++ Text2Speech.py
@@ -0,0 +1,7 @@
+# Text to Speech conversion using Python #
+## Prequities** > Install Python Library "pyttsx3" **
+
+import pyttsx3
+Speech = pyttsx3.init()
+Speech.say("Please join Python Developer Community")
+Speech.runAndWait()
